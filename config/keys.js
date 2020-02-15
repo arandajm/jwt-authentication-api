e2e3ac52d1fafc712ea37fb871c0f00b0ebbc399
Jesus Aranda <aranda.jm@icloud.com>
2020-02-14 21:38:16 -0300

    Connecting To MongoDB With Mongoose

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb://jesus:123456a@ds011422.mlab.com:11422/jwtauthenticationapi"
+};
--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+// DB config keys
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB with Mongoose
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected!"))
+  .catch(err => console.error(err));
 
 app.get("/", (req, res) => {
   res.send("Hello!!");
