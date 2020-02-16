c7bce7c4306b8d9a501f761834208ec4707bd733
Jesus Aranda <aranda.jm@icloud.com>
2020-02-16 11:21:52 -0300

    Creating the mongoose user model and schema

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create Schema
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now()
+  }
+});
+
+// Create new model with the UserSchema and export it
+module.exports = user = mongoose.model("users", UserSchema);
